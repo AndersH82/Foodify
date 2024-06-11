756175d984bf12d1b7cc58a9f220b0c90abc6678
AndersH82 <andersthereseholst@gmail.com>
2024-06-11 11:21:53 +0200

    settings

--- foodify/settings.py
+++ foodify/settings.py
@@ -18,7 +18,6 @@ load_dotenv()
 import os
 import dj_database_url
 
-print(os.getenv("MY_KEY"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
