8ac79e2741b9aef24f11cfea30e83e019375ea6b
AndersH82 <andersthereseholst@gmail.com>
2024-06-11 10:52:13 +0200

    settings

---
+++ env.py
@@ -0,0 +1,2 @@
+SECRET_KEY='django-insecure-@apba4)i_8uc$%de+0o_37cpmb&k(%52zn!lu1sc1ddnfyz)v!'
+ALLOWED_HOSTS=['https://swefoodify-dd39213e100e.herokuapp.com/', 'localhost', '127.0.0.1']
--- foodify/settings.py
+++ foodify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@apba4)i_8uc$%de+0o_37cpmb&k(%52zn!lu1sc1ddnfyz)v!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+LLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Set session to expire at browser close
